455db0405403257d162715b862a4c046e12e79b8
daheins <daheins@gmail.com>
2014-07-26 14:31:49 -0700

    Initial Commit

---
+++ hottub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hottub
+//
+//  Created by Daniel Heins on 7/26/14.
+//  Copyright (c) 2014 daheins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ hottubTests/hottubTests.m
@@ -0,0 +1,34 @@
+//
+//  hottubTests.m
+//  hottubTests
+//
+//  Created by Daniel Heins on 7/26/14.
+//  Copyright (c) 2014 daheins. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hottubTests : XCTestCase
+
+@end
+
+@implementation hottubTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
